922af43f02964c26e95d6704e074e72a2b17cd35
Class/MOhan <mohan.lar96@gmail.com>
2021-01-28 12:28:17 -0600

    my-first-app @angular

--- Assignment9/MeanGame/index.js
+++ Assignment9/MeanGame/index.js
@@ -9,9 +9,11 @@ var routes = require("./api/route");
 const app = express();
 app.set("port", 3000);
 
-app.use(function(req, res, next) {    
-    console.log(req.method, req.url);
+app.use("/api",function(req,res,next){
+    res.header("Access-Control-Allow-Origin","http://localhost:4200");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
     next();
+
 });
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -19,6 +21,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+
 
 app.use("/api", routes);
 
